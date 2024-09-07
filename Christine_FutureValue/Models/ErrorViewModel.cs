0d53c7f5acc39435f678ebc62881cee26a7d01af
Christine Hoang <christinehoangt@gmail.com>
2024-09-07 12:23:39 -0500

    Add project files.

---
+++ Christine_FutureValue/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Christine_FutureValue.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
