a9c514054d6b579c0592c8274314204d370e29b1
Christine Hoang <christinehoangt@gmail.com>
2024-09-07 13:33:25 -0500

    the template information has been deleted. Controller includes, age, name, and currency. View has html and razor blocks.

--- Christine_FutureValue/Controllers/HomeController.cs
+++ Christine_FutureValue/Controllers/HomeController.cs
@@ -1,32 +1,15 @@
-using Christine_FutureValue.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Christine_FutureValue.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
+            ViewBag.Name = "Christine";
+            ViewBag.FV = 1000;
+            ViewBag.Age = 35;
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Christine_FutureValue/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Christine_FutureValue.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
