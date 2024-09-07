440c219b6145b135d6dda6482d8c538d714e8852
Christine Hoang <christinehoangt@gmail.com>
2024-09-07 13:54:56 -0500

    fixed syntax erriors in Models. Stopped at 40.14 in demo video

---
+++ Christine_FutureValue/Models/FutureValueModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace Christine_FutureValue.Models
+{
+    public class FutureValueModel
+    {
+        public decimal MonthlyInvestment { get; set; }
+        public decimal  YearlyInterestRate { get; set; }
+        public int Years { get; set; }
+
+        public decimal CalculateFutureValue()
+        {
+            int months = Years * 12;
+            decimal monthlyInetredtRate = YearlyInterestRate / 12 / 100;
+
+            decimal futureValue = 0;
+
+            for (int i = 0; i < months; i++)
+                futureValue = (futureValue + MonthlyInvestment);
+
+     
+
+        }
+}
+}
