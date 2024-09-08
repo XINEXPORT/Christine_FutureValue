7963f033ef6f11a833fe50ab5816db117229edf0
Christine Hoang <christinehoangt@gmail.com>
2024-09-07 23:00:06 -0500

    FutureValueModel done

--- Christine_FutureValue/Models/FutureValueModel.cs
+++ Christine_FutureValue/Models/FutureValueModel.cs
@@ -5,21 +5,20 @@ namespace Christine_FutureValue.Models
     public class FutureValueModel
     {
         public decimal MonthlyInvestment { get; set; }
-        public decimal  YearlyInterestRate { get; set; }
+        public decimal YearlyInterestRate { get; set; }
         public int Years { get; set; }
 
         public decimal CalculateFutureValue()
         {
             int months = Years * 12;
-            decimal monthlyInetredtRate = YearlyInterestRate / 12 / 100;
-
+            decimal monthlyInterestRate = YearlyInterestRate / 12 / 100;
             decimal futureValue = 0;
 
             for (int i = 0; i < months; i++)
-                futureValue = (futureValue + MonthlyInvestment);
-
-     
-
+            {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+            return futureValue; 
         }
-}
+    }
 }
